ff2105f21fe05c2918742927159c26f59b97b30b
Sakshi <>
2022-03-29 11:22:23 +0530

    fix: navbar improved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 
+import {Route,Routes} from "react-router-dom"
+
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Routes>
+        {/* <Route path="/" element={<Home/>}  /> */}
+      </Routes>
       <h1>Hllo</h1>
     </div>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,13 +8,19 @@ const Navbar = () => {
     <div className="type-one--navbar">
   <div className="top-nav">
      <img src="https://cdn.dribbble.com/users/22136/screenshots/10873421/media/cec33efe113c8f7b0b999c682fe174fd.jpg?compress=1&resize=400x300&vertical=top" alt="logo-company" className="company-logo" />
-     <i className="fas fa-bars"></i>
      <p>Kairo Stream</p>
+     <i className="fas fa-bars"></i>
   </div>
   <div className="hide">
      <div className="top-links">
      <Link to="/login">
           <button className="btn">Login</button>
+        </Link>
+        <Link to="/signup">
+          <button className="btn">SignUp</button>
+        </Link>
+        <Link to="/gallery">
+          <button className="btn">Gallery</button>
         </Link>
         </div>
   </div>
