f7ecd219135e7a0fa6862c0add3fb30acf46e7e3
Sakshi <>
2022-03-29 11:31:59 +0530

    Revert "fix: navbar improved"
    This reverts commit ff2105f21fe05c2918742927159c26f59b97b30b.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 
-import {Route,Routes} from "react-router-dom"
-
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Routes>
-        {/* <Route path="/" element={<Home/>}  /> */}
-      </Routes>
       <h1>Hllo</h1>
     </div>
   );
