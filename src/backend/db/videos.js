5f6cac5b0fcdf03dc4f293acba23de06ac069d36
Sakshi <>
2022-03-30 12:01:56 +0530

    feat: video listing and filter feature

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,7 +5,7 @@
 
  export const videos = [
   {
-    _id:"00141316-67a5-45e6-ab04-e5c324c614da",
+    _id:"Hkc0ji-b4xA&ab_channel=KingSwish",
     title: "NBA Basketball Genius MOMENTS",
     creator: "KingSwish",
     published: "Nov 4, 2020",
@@ -14,7 +14,7 @@
     thumbnail:"https://images.unsplash.com/photo-1574142233428-93b8c48af5ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFza2V0YmFsbCUyMG1hdGNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
   },
   {
-    _id:"a50ba3ec-6190-4eac-85cd-cb83552bf069",
+    _id:"8YSrNfcKvA0&ab_channel=Olympics",
     title: "France 🇫🇷 vs USA 🇺🇸 | Men's Basketball Gold Medal Match | Tokyo Replays",
     creator: "Olympics",
     published:"Aug 19, 2021",
@@ -23,7 +23,7 @@
     thumbnail:"https://images.unsplash.com/photo-1561917443-6c5a9a4fca6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDl8fGJhc2tldGJhbGwlMjBtYXRjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
   },
   {
-    _id:"b5eec6f9-646a-4192-bef1-4828cb437bce",
+    _id:"6n7AfOtjIbo&ab_channel=ProTv",
     title: "Top 10 One Handed Catch in Cricket History",
     creator: "Pro Tv",
     published: "Jan 1, 2022",
@@ -32,7 +32,7 @@
     thumbnail:"https://images.unsplash.com/photo-1624194697120-34347cff8b58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNyaWNrZXQlMjBtYXRjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
   },
   {
-    _id:"5779e78b-191c-4149-a22f-b5b0ddfb1ef9",
+    _id:"Lz5098-SeFY&ab_channel=England%26WalesCricketBoard",
     title: "LAST BALL Thriller! | England v South Africa 2017 T20 Classic | England Cricket 2020",
     creator: "England & Wales Cricket Board",
     published:"Feb 16, 2020",
@@ -41,7 +41,7 @@
     thumbnail:"https://images.unsplash.com/photo-1607734834519-d8576ae60ea6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y3JpY2tldCUyMG1hdGNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
   },
   {
-    _id:"670a4ce5-315f-4a11-980e-c429f6e0de57",
+    _id:"HucIqi8Lw3E&ab_channel=Olympics",
     title: "Saina Nehwal Wins Badminton Women's Singles Bronze - IND v CHN | London 2012 Olympics",
     creator: "Olympics",
     published: "Aug 4, 2012",
@@ -50,7 +50,7 @@
     thumbnail:"https://images.unsplash.com/photo-1622365070739-8c71a665353c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTh8fGJhZG1pbnRvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
   },
   {
-    _id:"8716718d-91cf-4e64-aa37-e9a5497d1ccb",
+    _id:"O6VqAG6b--o&ab_channel=ShuttleFlash",
     title: "10 IMPOSSIBLE Badminton Moments of 2021",
     creator: "Shuttle Flash",
     published:"Jun 27, 2021",
@@ -59,7 +59,7 @@
     thumbnail:"https://images.unsplash.com/flagged/photo-1572987337657-0a3c2872f044?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njd8fGJhZG1pbnRvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
   },
   {
-    _id:"a6afcf48-cb85-4bb1-a7b7-e9b14c97dd6e",
+    _id:"2FE-3A9-If4&ab_channel=FOOT4K",
     title: "Superhuman Skills In Football!",
     creator: "FOOT4K",
     published: "Mar 28, 2022",
@@ -68,7 +68,7 @@
     thumbnail:"https://images.unsplash.com/photo-1600250395178-40fe752e5189?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Zm9vdGJhbGwlMjBtYXRjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
   },
   {
-    _id:"984ed81f-c6ca-4c11-b27f-c770979eea4d",
+    _id:"ZEBVQ2Y0QiU&ab_channel=Score90",
     title: "Comedy Moments in Football",
     creator: "Score 90",
     published:"Mar 2, 2022",
