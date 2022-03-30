377eafb11866b9b74a223e7fe5e4bf2dd455e4b0
Sakshi <>
2022-03-30 15:26:51 +0530

    feat: notfound page added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./Pages/Home/Home";
 import Mockman from "mockman-js";
 import { Login } from "./Pages/Authentication/Login";
 import Signup from "./Pages/Authentication/Signup";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home/>}  />
         <Route path="/login" element={<Login/>}  />
         <Route path="/signup" element={<Signup/>}  />
+        <Route path="*" element={<NotFound />} />
         <Route path="mockman" element={<Mockman/>} />
       </Routes>
     </div>
