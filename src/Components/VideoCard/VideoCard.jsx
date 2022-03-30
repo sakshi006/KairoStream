0eb91a82947ca1cf5fb13efd2fad5f56e1df510e
Sakshi <>
2022-03-30 11:00:44 +0530

    feat: video listing and filter feature

--- src/Components/VideoCard/VideoCard.jsx
+++ src/Components/VideoCard/VideoCard.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./VideoCard.css";
 import { BsFillClockFill } from "react-icons/bs";
+import { AiOutlinePlayCircle } from "react-icons/ai";
 
 const VideoCard = ({item}) => {
   return (
     <div className="cardcom background">
       <div className="card">
-        <img
+       <div className="text-over-video">
+         <AiOutlinePlayCircle className="play-btn"/>
+       <img
           className="card-image-one"
           src={item.thumbnail}
           alt="card"
         />
+       </div>
         <article className="card-text-one">
           <figcaption>{item.title}</figcaption>
           <h5>{item.creator}</h5>
